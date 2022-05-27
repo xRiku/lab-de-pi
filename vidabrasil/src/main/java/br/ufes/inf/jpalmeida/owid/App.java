b945e3c4266bcf7bfa9877febdadcc304dc86d82
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2022-05-26 21:16:34 -0300

    Feito ate o roteiro 2

--- vidabrasil/src/main/java/br/ufes/inf/jpalmeida/owid/App.java
+++ vidabrasil/src/main/java/br/ufes/inf/jpalmeida/owid/App.java
@@ -3,6 +3,7 @@ package br.ufes.inf.jpalmeida.owid;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Collections;
 import java.util.Vector;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
@@ -23,9 +24,9 @@ public class App
                 String entity = record.get(0);
                 String code = record.get(1);
                 Integer year = Integer.parseInt(record.get(2));
-                String male = record.get(3);
-                String female = record.get(4);
-                System.out.printf("%s %s: %d %s - %s\n", code, entity, year, male, female);
+                Double male = Double.parseDouble(record.get(3));
+                Double female = Double.parseDouble(record.get(4));
+                // System.out.printf("%s %s: %d %s - %s\n", code, entity, year, male, female);
                 data d = new data(entity, code, year, male, female);
                 v.add(d);
             }
@@ -35,11 +36,24 @@ public class App
         return v;
     }
 
-    public void getExpectativaPorPais(Integer[] datas) {
-        ;
+    static public void getExpectativaPorPaisEmUmAno(Vector<data> dt, Integer n) {
+        Vector<data> ndt = new Vector<data>();
+        for(data d: dt) {
+            // System.out.println(n.equals(d.getYear()));
+            if (n.equals(d.getYear())) {
+                ndt.add(d);
+            }
+            // System.out.println(d.entity);
+        }
+        Collections.sort(ndt);
+        for(data nd: ndt) {
+            System.out.printf("%s: %.2f - %.2f\n", nd.getEntity(), nd.getMale(), nd.getFemale());
+        }
     }
     public static void main( String[] args )
     {
-        Vector<data> v = readFileCSV("resources/file.csv");
+        Vector<data> dt = readFileCSV("resources/file.csv");
+        getExpectativaPorPaisEmUmAno(dt, 2020);
+        
     }
 }
--- vidabrasil/src/main/java/br/ufes/inf/jpalmeida/owid/data.java
+++ vidabrasil/src/main/java/br/ufes/inf/jpalmeida/owid/data.java
@@ -1,19 +1,61 @@
 package br.ufes.inf.jpalmeida.owid;
 
-public class data {
-    String entity;
-    String code;
-    Integer year;
-    String male;
-    String female;
+import java.util.Objects;
 
-    public data(String entity, String code, Integer year, String male, String female) {
+public class data implements Comparable<data> {
+    public String entity;
+    private String code;
+    private Integer year;
+    private Double male;
+    private Double female;
+
+    public data(String entity, String code, Integer year, Double male, Double female) {
         this.entity = entity;
         this.code = code;
         this.year = year;
         this.male = male;
         this.female = female;
     }
+
+    @Override
+    public int compareTo(data arg0) {
+        // TODO Auto-generated method stub
+        return -Double.compare(this.getMale() + this.getFemale(), arg0.getMale() + arg0.getFemale());
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getEntity() {
+        return entity;
+    }
+
+    public void setEntity(String entity) {
+        this.entity = entity;
+    }
+
+    public Double getMale() {
+        return male;
+    }
+
+    public void setMale(Double male) {
+        this.male = male;
+    }
+
+    public Double getFemale() {
+        return female;
+    }
+
+    public void setFemale(Double female) {
+        this.female = female;
+    }
+
+    
 
     
 }
